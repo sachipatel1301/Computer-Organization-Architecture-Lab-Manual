0658c1a6d1dcad82cf788fb1076a09ebd7533a52
sachi patel <sachikpatel13@gmail.com>
2025-01-28 12:14:47 +0530

    Updated code in filename.c with new features/bug fixes

--- Practical1a.c
+++ Practical1a.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 
 void decimalToBinary(int n) {
-    int binary[32]; // Array to store binary digits
+    int binary[32]; 
     int i = 0;
     
-    // Converting to binary
     while (n > 0) {
-        binary[i] = n % 2; // Store remainder
-        n = n / 2;         // Update the number
+        binary[i] = n % 2; 
+        n = n / 2;         
         i++;
     }
     
-    // Print the binary number in reverse order
     printf("Binary: ");
     for (int j = i - 1; j >= 0; j--) {
         printf("%d", binary[j]);
@@ -22,12 +20,11 @@ void decimalToBinary(int n) {
 int main() {
     int decimal;
 
-    // Take input from the user
     printf("Enter a decimal number: ");
     scanf("%d", &decimal);
 
     if (decimal >= 0) {
-        decimalToBinary(decimal); // Convert and display binary
+        decimalToBinary(decimal); 
     } else {
         printf("Please enter a non-negative integer.\n");
     }
